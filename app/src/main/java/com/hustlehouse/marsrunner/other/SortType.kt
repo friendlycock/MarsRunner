c4cb5f883eb65c8a0d5b6e4bed8c220f54d03ff6
friendlycock <pawok1@mail.ru>
2022-10-17 17:29:45 +0100

    RunningAppYT Initial Commit

---
+++ app/src/main/java/com/hustlehouse/marsrunner/other/SortType.kt
@@ -0,0 +1,5 @@
+package com.hustlehouse.marsrunner.other
+
+enum class SortType {
+    DATE, RUNNING_TIME, AVG_SPEED, DISTANCE, CALORIES_BURNED
+}
---
+++ app/src/main/java/com/hustlehouse/marsrunner/ui/viewmodels/StatisticsViewModel.kt
@@ -0,0 +1,18 @@
+package com.hustlehouse.marsrunner.ui.viewmodels
+
+import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.ViewModel
+import com.hustlehouse.marsrunner.repositories.MainRepository
+import javax.inject.Inject
+
+class StatisticsViewModel @ViewModelInject constructor(
+    val mainRepository: MainRepository
+): ViewModel() {
+
+    val totalTimeRun = mainRepository.getTotalTimeInMillis()
+    val totalDistance = mainRepository.getTotalDistance()
+    val totalCaloriesBurned = mainRepository.getTotalCaloriesBurned()
+    val totalAvgSpeed = mainRepository.getTotalAvgSpeed()
+
+    val runsSortedByDate = mainRepository.getAllRunsSortedByDate()
+}
